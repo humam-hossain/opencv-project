31b2356fb5b5fce0835e39d61125bcae430b0d2e
humam-hossain <humam.hossain.e@gmail.com>
2022-12-28 19:12:55 +0600

    checking it

--- learning_opencv/src/histogram.py
+++ learning_opencv/src/histogram.py
@@ -38,7 +38,7 @@ plt.xlabel("Bins")
 plt.ylabel("# of pixels")
 
 for i,col in enumerate(colors):
-    hist = cv.calcHist([img], [i], circle, [256], [0, 256])
+    hist = cv.calcHist([img], [i], None, [256], [0, 256])
     plt.plot(hist, color=col)
     plt.xlim([0, 256])
 
